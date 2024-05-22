6a518bfef3a5addfa20b76439ecb8841d51596af
Resousse <11071770+Resousse@users.noreply.github.com>
2024-05-22 15:49:27 +0200

    avoid several resize

--- fillBackground.js
+++ fillBackground.js
@@ -62,8 +62,6 @@ function fillBackground(idCanvas, color, icons, opacity, size = 100, minMargin =
     window.onresize = function (event) {
         if (toRefresh && document.body.scrollTop >= 0)
             {
-                console.log("scrollTop" + document.body.scrollTop);
-                console.log("scrollHeight" + document.body.scrollHeight);
                 fillBackground(idCanvas, color, icons, opacity, size, minMargin, staticOrientation);
             }
     };
@@ -114,6 +112,10 @@ function fillBackgroundOnceLoaded(idCanvas, color, icons, opacity, size = 100, m
     canvas.height = window.screen.height * ratio;
     canvas.style.position = "fixed";
     canvas.style.zIndex = "-1";
+    if (typeof canvasHeight !== "undefined" && typeof canvasWidth !== "undefined" && canvasWidth == canvas.width && canvasHeight == canvas.height)
+        return;
+    canvasHeight = canvas.height;
+    canvasWidth = canvas.width;
     
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
